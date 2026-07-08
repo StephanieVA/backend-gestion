45c9676bd8b2cfd7b31768c4cb7d55fd8cf5c635
StephanieVA <stephanie1104@outlook.com>
2026-07-07 23:39:49 -0500

    Probando

--- server.js
+++ server.js
@@ -6,26 +6,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-//app.get("/", (req, res) => {
-// res.send("Servidor funcionando");
-//});
-app.use((req, res, next) => {
-  console.log("Petición:", req.method, req.url);
-  next();
-});
 app.get("/", (req, res) => {
-  console.log("Solicitud recibida en /");
-  res.send("Servidor funcionando");
+  console.log("Recibida petición /");
+  res.status(200).send("Servidor funcionando correctamente");
 });
-
-//
-app.use("/api/auth", require("./routes/auth.routes"));
-app.use("/api/respuestas", require("./routes/respuestas.routes"));
-app.use("/api/reportes", require("./routes/reportes.routes"));
-app.use("/api/cursos", require("./routes/cursos.routes"));
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Servidor iniciado en puertos ${PORT}`);
+  console.log(`Servidor iniciado en puerto ${PORT}`);
 });
