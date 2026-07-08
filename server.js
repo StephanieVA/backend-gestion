5111a6ee21fe656079e057c06f62462047802cc9
StephanieVA <66794568+StephanieVA@users.noreply.github.com>
2026-07-08 12:43:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors({
+  origin: [
+    "https://gestion-tiempo-phi.vercel.app",
+    "http://localhost:4200"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  console.log("RECIBIDA PETICION ROOT");
-  res.status(200).send("OK Railway");
+  res.send("Servidor funcionando");
 });
 
-const PORT = process.env.PORT || 3000;
+app.use("/api/auth", require("./routes/auth.routes"));
+app.use("/api/respuestas", require("./routes/respuestas.routes"));
+app.use("/api/reportes", require("./routes/reportes.routes"));
+app.use("/api/cursos", require("./routes/cursos.routes"));
 
-console.log("PORT ASIGNADO:", PORT);
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor iniciado en puerto ${PORT}`);
