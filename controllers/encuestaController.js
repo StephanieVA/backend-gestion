c0cbf615978778008afa02245fa7e6392cdb2d3c
StephanieVA <66794568+StephanieVA@users.noreply.github.com>
2026-07-24 00:35:40 -0500

    Update encuestaController.js

--- controllers/encuestaController.js
+++ controllers/encuestaController.js
@@ -57,3 +57,121 @@ exports.obtenerCursos = async (req, res) => {
 exports.registrarEncuesta = async (req, res) => {
 
 };
+const db = require("../config/db");
+
+
+exports.guardarEncuesta = async (req, res) => {
+
+
+const connection = await db.getConnection();
+
+
+try {
+
+
+await connection.beginTransaction();
+
+
+
+const {
+persona,
+respuestas
+} = req.body;
+
+
+
+// 1. Guardar estudiante
+
+await connection.query(
+
+`
+INSERT INTO estudiantes_encuesta
+(
+dni,
+apellidos_nombres,
+edad,
+sexo,
+semestre,
+seccion
+)
+VALUES (?,?,?,?,?,?)
+
+`,
+
+[
+
+persona.dni,
+persona.apellidos_nombres,
+persona.edad,
+persona.sexo,
+persona.semestre,
+persona.seccion
+
+]
+
+);
+
+
+
+
+// 2. Crear encuesta
+
+const [encuesta] = await connection.query(
+
+`
+INSERT INTO encuesta_tutoria
+(
+dni
+)
+VALUES (?)
+
+`,
+
+[
+persona.dni
+]
+
+);
+const idEncuesta = encuesta.insertId;
+// 3. Guardar respuestas
+for(const r of respuestas){
+await connection.query(
+`
+INSERT INTO encuesta_tutoria_respuestas
+(
+id_encuesta,
+id_curso,
+recibio,
+tipo,
+modalidad
+)
+VALUES (?,?,?,?,?)
+
+`,
+
+[
+
+idEncuesta,
+r.idCurso,
+r.recibio,
+r.tipo,
+r.modalidad
+]
+);}
+await connection.commit();
+res.json({
+mensaje:"Encuesta guardada correctamente"
+});
+}
+catch(error){
+await connection.rollback();
+console.log(error);
+res.status(500).json({
+mensaje:"Error al guardar encuesta",
+error:error.message
+});
+}
+finally{
+connection.release();
+}
+};
