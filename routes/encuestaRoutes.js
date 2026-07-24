cf850b4f28477691457f0fb46a37c541ba9890e6
StephanieVA <66794568+StephanieVA@users.noreply.github.com>
2026-07-23 22:14:53 -0500

    Add files via upload

---
+++ routes/encuestaRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+const encuestaController = require("../controllers/encuestaController");
+
+// Validar si el DNI ya existe
+router.get("/validar-dni/:dni", encuestaController.validarDni);
+
+// Obtener cursos por semestre
+router.get("/cursos/:semestre", encuestaController.obtenerCursos);
+
+// Registrar encuesta completa
+router.post("/registrar", encuestaController.registrarEncuesta);
+
+module.exports = router;
