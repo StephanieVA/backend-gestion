b48b1a2d1484f8acddf8ee6deadf76332992100f
StephanieVA <66794568+StephanieVA@users.noreply.github.com>
2026-07-23 22:17:25 -0500

    Update encuestaController.js

---
+++ controllers/encuestaController.js
@@ -0,0 +1,16 @@
+const db = require("../config/db");
+
+// Validar DNI
+exports.validarDni = async (req, res) => {
+
+};
+
+// Obtener cursos
+exports.obtenerCursos = async (req, res) => {
+
+};
+
+// Registrar encuesta
+exports.registrarEncuesta = async (req, res) => {
+
+};
