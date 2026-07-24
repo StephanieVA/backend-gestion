51b7117d645ab67a477c3be5a8ee1ba466574bac
StephanieVA <66794568+StephanieVA@users.noreply.github.com>
2026-07-23 22:32:18 -0500

    Update encuestaController.js

--- controllers/encuestaController.js
+++ controllers/encuestaController.js
@@ -1,11 +1,22 @@
 const db = require("../config/db");
 
 // Validar DNI
-exports.validarDni = (req, res) => {
-    res.json({
-        mensaje: "Ruta funcionando",
-        dni: req.params.dni
-    });
+exports.validarDni = async (req, res) => {
+
+    console.log("Entró a validar DNI");
+
+    const { dni } = req.params;
+
+    console.log("DNI:", dni);
+
+    const [rows] = await db.query(
+        "SELECT * FROM estudiantes_encuesta WHERE dni=?",
+        [dni]
+    );
+
+    console.log(rows);
+
+    res.json(rows);
 };
 
 // Obtener cursos
